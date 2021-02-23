2f7b9cbafd658eb06c8b0a553a115fc6da937dc4
LT <tey_liang_tai@tech.gov.sg>
2021-02-23 10:45:48 +0800

    Revert "Revert "Clean up sgds custom css code, fixes for navbar and index page""
    This reverts commit 6dfc9881570676baa5155cbfc781a00347f752f1.

--- assets/js/common.js
+++ assets/js/common.js
@@ -294,7 +294,7 @@ if (!BLUEPRINT.isReady) {
 }
 
 // Get all "navbar-burger" elements
-var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.sgds-navbar-burger'), 0);
 
 // Check if there are any navbar burgers
 if ($navbarBurgers.length > 0) {
@@ -306,6 +306,8 @@ if ($navbarBurgers.length > 0) {
             // Get the target from the "data-target" attribute
             var target = $el.dataset.target;
             var $target = document.getElementById(target);
+            
+            console.log(target)
 
             // Toggle the class on both the "navbar-burger" and the "navbar-menu"
             $el.classList.toggle('is-active');
